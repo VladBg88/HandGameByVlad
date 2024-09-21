f743adfbaf648444cf9bc6a04f93613ad0bc56bc
Vladislav Stoilov <oxus.home@gmail.com>
2024-09-21 09:42:19 +0300

    Uploaded a version 2 of HandGameByVlad.

--- hand_game_by_vlad_v2.py
+++ hand_game_by_vlad_v2.py
@@ -24,32 +24,23 @@ while True:
 
     counter_first_input += 1
 
-    computer_random_number = random.randint(1, 3)
-    if computer_random_number == 1:
-        computer_move = rock
-    elif computer_random_number == 2:
-        computer_move = paper
-    else:
-        computer_move = scissors
+    choices = ['rock', 'paper', 'scissors']
+    computer_move = random.choice(choices)
+
+    print(f"\nYou chose {player_move}. The computer chose {computer_move}.")
 
     if player_move == computer_move:
         result = 'd'
+        print("It's a draw!")
+        counter_draw += 1
     elif (player_move == rock and computer_move == scissors) or \
             (player_move == paper and computer_move == rock) or \
             (player_move == scissors and computer_move == paper):
         result = 'w'
-    else:
-        result = 'l'
-
-    print(f"\nYou chose {player_move}. The computer chose {computer_move}.")
-
-    if result == 'w':
         print("You win this round!")
         counter_win += 1
-    elif result == 'd':
-        print("It's a draw!")
-        counter_draw += 1
     else:
+        result = 'l'
         print("You lose this round!")
         counter_lose += 1
 
